eb5bc2024bf460c9e5667983bcd86b6efb2af818
Rahul-404 <rahulshelke981@gmail.com>
2026-05-09 13:03:05 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple-calculator-rahul",
+    version="0.1.0",
+    author="Rahul",
+    description="A simple calculator package",
+    packages=find_packages(),
+    install_requires=[],
+)
