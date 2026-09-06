a94b024139c5feb5529e1037a8b0f4df5911a073
Rahul-404 <rahulshelke981@gmail.com>
2026-09-06 13:11:13 +0530

    feat(api): add Flask web application

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template, jsonify
+from calculator.operations import add, subtract, multiply, divide
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def main():
+    return render_template("index.html")
+
+
+@app.route("/math", methods=["POST"])
+def math():
+
+    data = request.get_json()
+
+    num1 = data["num1"]
+    num2 = data["num2"]
+    operation = data["operation"]
+
+    if operation == "add":
+        result = add(num1, num2)
+
+    elif operation == "subtract":
+        result = subtract(num1, num2)
+
+    elif operation == "multiply":
+        result = multiply(num1, num2)
+
+    elif operation == "divide":
+        result = divide(num1, num2)
+
+    else:
+        return jsonify({"error": "Invalid operation"}), 400
+
+    return jsonify({"result" : result})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
