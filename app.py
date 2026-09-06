d85785f7601cfb5e7fd6e6dd3ded691d577af4b5
Rahul-404 <rahulshelke981@gmail.com>
2026-09-06 18:31:03 +0530

    fix(api): turn off reloader

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def math():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
