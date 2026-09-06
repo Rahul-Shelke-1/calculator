f1d9a459652ec9ab13424d58c3dabe148dd9a898
Rahul-404 <rahulshelke981@gmail.com>
2026-09-06 16:14:57 +0530

    refactor(api): add try-catch and error code

--- app.py
+++ app.py
@@ -11,29 +11,31 @@ def main():
 
 @app.route("/math", methods=["POST"])
 def math():
+    try:
+        data = request.get_json()
 
-    data = request.get_json()
+        num1 = data["num1"]
+        num2 = data["num2"]
+        operation = data["operation"]
 
-    num1 = data["num1"]
-    num2 = data["num2"]
-    operation = data["operation"]
+        if operation == "add":
+            result = add(num1, num2)
 
-    if operation == "add":
-        result = add(num1, num2)
+        elif operation == "subtract":
+            result = subtract(num1, num2)
 
-    elif operation == "subtract":
-        result = subtract(num1, num2)
+        elif operation == "multiply":
+            result = multiply(num1, num2)
 
-    elif operation == "multiply":
-        result = multiply(num1, num2)
+        elif operation == "divide":
+            result = divide(num1, num2)
 
-    elif operation == "divide":
-        result = divide(num1, num2)
+        else:
+            return jsonify({"error": "Invalid operation"}), 400
 
-    else:
-        return jsonify({"error": "Invalid operation"}), 400
-
-    return jsonify({"result" : result})
+        return jsonify({"result" : result})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
